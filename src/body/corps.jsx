4d6fb3a573021d7ca1e769d1395a79060deeff95
Dev_Aurelien <lhuillier.aurelien47@gmail.com>
2025-09-01 21:22:14 +0200

    mise en place maquette

--- src/body/corps.jsx
+++ src/body/corps.jsx
@@ -1,12 +1,18 @@
 export default function Corps() {
   return (
     <div className="flex w-[70%] h-full bg-zinc-950 rounded-2xl">
-      <img src="/Seraphim.jpg" alt="Seraphim" >
-      <input
-        className="flex mt-2 border-2 border-white/30 text-white rounded-full bg-zinc-950 h-[3rem] w-full p-4 justify-center items-center"
-        type="text"
-        placeholder="Stations"
-      /></img>
+      <div className="relative w-full aspect-square">
+        <img
+          className="w-full h-full object-cover rounded-2xl"
+          src="/Checkmate.png"
+          alt="Checkmate"
+        />
+        <input
+          className="absolute bottom-4 left-1/2 -translate-x-1/2 bg-zinc-950 text-white rounded-full px-4 py-2 shadow-md z-10"
+          type="text"
+          placeholder="Stations"
+        />
+      </div>
     </div>
   );
 }
