43180662a59ad1c6bb8873d6b70bec481bd71f2b
Dev_Aurelien <lhuillier.aurelien47@gmail.com>
2025-10-28 20:36:02 +0100

    card stations

--- src/body/navLieux.jsx
+++ src/body/navLieux.jsx
@@ -5,7 +5,7 @@ export default function NavLieuxnav() {
     <nav className="flex flex-col gap-4 justify-start w-[10%] h-full ml-[0.2rem] p-2 bg-zinc-950 rounded-2xl">
       {/* <div><input className="bg-white rounded-xl w-full p-1 my-[0.2rem]" type="text"/></div> */}
       <button className="bg-zinc-800 rounded-xl">Ajouter</button>
-      <ul className="flex flex-col gap-4 list-none text-white"> <li className="flex"><CardStation/></li><li><CardStation/></li><li><CardStation/></li><li><CardStation/></li></ul>
+      <div className="flex flex-col gap-4 list-none text-white"><CardStation station_name="Station 1"/></div> 
     </nav>
   );
 }
--- src/components/CardStation.jsx
+++ src/components/CardStation.jsx
@@ -1,7 +1,7 @@
-export default function CardStation() {
+export default function CardStation({ station_name }) {
   return (
     <div className="flex m-auto w-full aspect-square bg-stone-600 rounded-xl justify-center items-center">
-      station
+      <div className="relative h-full w-full"><img className="object-cover h-full w-full rounded-xl" src="https://media.starcitizen.tools/thumb/1/13/ARC-L1-Exterior-Alpha3.12.png/400px-ARC-L1-Exterior-Alpha3.12.png.webp" alt="" /><span className="absolute top-2 left-4 text-white">{station_name}</span></div>
     </div>
   );
 }
