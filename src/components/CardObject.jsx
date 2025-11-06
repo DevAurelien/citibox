94567c028963f8d0ce10c5ba63239976656e83e5
Dev_Aurelien <lhuillier.aurelien47@gmail.com>
2025-11-06 22:11:31 +0100

    image classe

--- src/components/CardObject.jsx
+++ src/components/CardObject.jsx
@@ -11,13 +11,17 @@ export default function CardObject({composant}) {
 
   const title = composant?.Name ?? composant?.name ?? "Nom inconnu";
   const manufacturer = composant?.Manufacturer ?? composant?.manufacturer ?? "";
+  const size = composant?.Size ?? composant?.size ?? "";
+  const grade = composant?.Grade ?? composant?.grade ?? "";
+  const classe = composant?.Classe ?? composant?.classe ?? "";
+  composant && console.log(composant);
+  const caracteristiques = [size? `Taille : ${size}` : ""  ,grade ? ` Grade : ${grade} `:"\n", classe? `Classe : ${classe}` : "\n"  ];
 
-  // debug : vérifie l'objet reçu dans la console du navigateur
   // console.log("CardObject reçu:", composant);
 
   return (
     <div className="flex flex-col my-2 w-full bg-zinc-800 rounded-2xl p-3 text-white">
-      <img className="rounded-2xl mb-2" src={composant?.image_url || "./Panther.png"} alt="Objet" />
+      <div className="flex gap-2"><img className="rounded-2xl mb-2 size-[20vh]" src={composant?.image_url || "./Panther.png"} alt="Objet" /><ul>{caracteristiques}</ul></div>
       <h3 className="text-lg font-semibold">{title}</h3>
       {manufacturer && <p className="text-sm text-white/70">{manufacturer}</p>}
     </div>
